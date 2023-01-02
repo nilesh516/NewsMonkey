eaaa194627dc495ed2d5bcdc96343071bd956507
Nilesh Kumar Singh <singhnilesh516@gmail.com>
2023-01-02 20:57:18 +0530

    b

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
 
     let {title , description, imageUrl, newsUrl} = this.props;
 
-    return (
+    return ( 
       <div className='my-3'>
             <div className="card" style={{width: "18rem"}}>
             <img src={imageUrl} className="card-img-top" alt="..."/>
