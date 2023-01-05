820d51d1a6dfe6879e3aa33cd3251a1a8227774e
Nilesh Kumar Singh <singhnilesh516@gmail.com>
2023-01-05 17:22:41 +0530

    Class based completion
    Now I'll change the project using the function based component

--- src/components/News.js
+++ src/components/News.js
@@ -1,42 +1,116 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
 
 export class News extends Component {
 
-  constructor(){
-    super();
+  static defaultProps = {
+    country : 'in',
+    pageSize : 8,
+    category : 'general',
+  }
+
+  static propTypes = {
+    country : PropTypes.string,
+    pageSize : PropTypes.number,
+    category : PropTypes.string,
+  }
+   capitalize(strings){
+    return strings[0].toUpperCase() + strings.slice(1)
+   }
+
+  constructor(props){
+    super(props);
     this.state = {
       articles : [],
-      loading : false
+      loading : false,
+      page : 1,
 
     }
+    document.title = `${this.capitalize(this.props.category)} - NewsMonkey`
+  }
+
+  async updatePage(){
+    this.props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading : true})
+    let data = await fetch(url);
+    this.props.setProgress(40);
+    let parseData = await data.json();
+    this.props.setProgress(60);
+    this.setState({articles : parseData.articles, 
+      totalResults : parseData.totalResults,
+      loading:false,
+    })
+    this.props.setProgress(100);
+
   }
 
  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=12485d459295459f82a4ce9e683c712f";
+  this.updatePage();
+
+  }
+
+  handlePrevClick =async ()=>{
+    this.setState({page : this.state.page - 1,},()=>{this.updatePage()});
+
+  }
+  handleNextClick =async ()=>{
+    this.setState({page : this.state.page + 1},()=>{this.updatePage()});
+
+  }
+  fetchMoreData = async ()=>{
+    this.setState({page : this.state.page + 1})
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading : true})
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({articles : parseData.articles})
-    console.log(parseData)
+    this.setState({articles :this.state.articles.concat(parseData.articles), 
+      totalResults : parseData.totalResults,
+    })
 
   }
 
   render() {
     return (
-      <div className='container my-3'>
-      <h2>News Monkey - Top Headlines</h2>
+      <>
+      {/* <div className='container my-3'> */}
+      <h2 className='text-center' style={{margin : '30px 0px'}}>News Monkey - Top {this.capitalize(this.props.category)} Headlines</h2>
+      {/* {this.state.loading && <Spinner/>} */}
+      <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.totalResults!==this.state.articles.length}
+          loader={this.state.totalResults!==this.state.articles.length?<Spinner/>:''}
+        >
+        <div className="container">
         <div className="row my-3">
-          {this.state.articles.map((elements)=>{
+          {/*!this.state.loading && */this.state.articles.map((elements)=>{
             return(
             <div className="col-md-4" key={elements.url}>
-                <NewsItem title={elements.title?elements.title.slice(0,20):""} description={elements.description?elements.description.slice(0,80):""} imageUrl={elements.urlToImage} newsUrl={elements.url} />
+                <NewsItem title={elements.title?elements.title.slice(0,20):""} 
+                         description={elements.description?elements.description.slice(0,80):""} 
+                         imageUrl={elements.urlToImage} 
+                         newsUrl={elements.url}
+                         author={elements.author} 
+                         date={elements.publishedAt} 
+                         source={elements.source.name} />
             </div>)
-
-
           })}
             
         </div>
-      </div>
+        </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-outline-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+            <button disabled={this.state.page>=Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-outline-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          </div>  */}
+      {/* </div> */}
+      </>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   render() {
 
-    let {title , description, imageUrl, newsUrl} = this.props;
+    let {title , description, imageUrl, newsUrl, author, date, source} = this.props;
 
     return ( 
+      <>
       <div className='my-3'>
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card">
+            <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger" style={{zIndex : 1}}>{source}</span>
             <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
+                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm  btn-outline-dark">Read More</a>
             </div>
-            </div>        
+            </div>       
       </div>
+      </>
     )
   }
 }
